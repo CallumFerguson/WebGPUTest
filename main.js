d3d50e6b09ff1b0e1aa01e4b459718057ce5d51a
Callum Ferguson <Calxferg@gmail.com>
2024-02-14 16:55:16 -0800

    fix fail message

--- main.js
+++ main.js
@@ -380,10 +380,7 @@ async function main() {
 }
 
 function fail(msg) {
-  const elem = document.querySelector("#fail");
-  const contentElem = elem.querySelector(".content");
-  elem.style.display = "";
-  contentElem.textContent = msg;
+  console.log(msg);
 }
 
 main();
