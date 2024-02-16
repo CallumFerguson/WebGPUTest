3fa50625c8183b47d7d1a3c932e899ef55c6fbf0
Callum Ferguson <Calxferg@gmail.com>
2024-02-16 15:43:54 -0800

    test button

--- main.js
+++ main.js
@@ -14,7 +14,9 @@ function createBuffer(device, data, usage) {
 
 async function main() {
   const gpu = navigator.gpu;
-  const adapter = await gpu?.requestAdapter();
+  const adapter = await gpu?.requestAdapter({
+    powerPreference: "high-performance",
+  });
   const device = await adapter?.requestDevice();
   if (!device || !gpu) {
     console.log("browser does not support WebGPU");
@@ -29,12 +31,17 @@ async function main() {
   context.configure({
     device,
     format: presentationFormat,
+    alpha: "opaque",
   });
 
   const positions = new Float32Array([0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1]);
   const indices = new Uint16Array([0, 1, 2, 0]); // added 0 padding so it is a multiple of 8
 
-  const positionBuffer = createBuffer(device, positions, GPUBufferUsage.VERTEX);
+  const positionBuffer = createBuffer(
+    device,
+    positions,
+    GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
+  );
   const indicesBuffer = createBuffer(device, indices, GPUBufferUsage.INDEX);
 
   const shaderModule = device.createShaderModule({
@@ -75,16 +82,48 @@ async function main() {
     ],
   };
 
-  const commandEncoder = device.createCommandEncoder();
-  const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+  let buttonClicked = false;
+  document.querySelector("button").onclick = () => {
+    renderPassDescriptor.colorAttachments[0].clearValue = {
+      r: 0.0,
+      g: 1.0,
+      b: 0.0,
+      a: 1.0,
+    };
 
-  passEncoder.setPipeline(pipeline);
-  passEncoder.setVertexBuffer(0, positionBuffer);
-  passEncoder.setIndexBuffer(indicesBuffer, "uint16");
-  passEncoder.drawIndexed(Math.floor(indices.length / 3) * 3);
+    buttonClicked = true;
+  };
 
-  passEncoder.end();
-  device.queue.submit([commandEncoder.finish()]);
+  let previousTime = 0;
+  function render(currentTime) {
+    currentTime *= 0.001;
+    const deltaTime = currentTime - previousTime;
+
+    renderPassDescriptor.colorAttachments[0].view = context
+      .getCurrentTexture()
+      .createView();
+
+    const commandEncoder = device.createCommandEncoder();
+    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+
+    if (buttonClicked) {
+      const data = new Float32Array([0.5, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1]);
+      device.queue.writeBuffer(positionBuffer, 0, data, 0, data.length);
+      buttonClicked = false;
+    }
+
+    passEncoder.setPipeline(pipeline);
+    passEncoder.setVertexBuffer(0, positionBuffer);
+    passEncoder.setIndexBuffer(indicesBuffer, "uint16");
+    passEncoder.drawIndexed(Math.floor(indices.length / 3) * 3);
+
+    passEncoder.end();
+    device.queue.submit([commandEncoder.finish()]);
+
+    previousTime = currentTime;
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
 }
 
 main();
